f45636d2bf3e34338ab50f3de7bd5d86b2f526fe
planetchili <planetchili@users.noreply.github.com>
2016-07-20 13:45:36 +0900

    just some exception text

--- Engine/Main.cpp
+++ Engine/Main.cpp
@@ -43,7 +43,7 @@ int WINAPI wWinMain( HINSTANCE hInst,HINSTANCE,LPWSTR pArgs,INT )
 	}
 	catch( ... )
 	{
-		MessageBox( nullptr,L"\n\nException caught in Windows creation.",
+		MessageBox( nullptr,L"\n\nException caught at main window creation.",
 			L"Unhandled Non-STL Exception",MB_OK );
 	}
 
