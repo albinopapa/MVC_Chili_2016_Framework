c9bd68dfb9175decc59ce59895e79a5522a98c63
planetchili <planetchili@users.noreply.github.com>
2016-07-20 13:48:49 +0900

    added std::exception specific handler for main window creation

--- Engine/Main.cpp
+++ Engine/Main.cpp
@@ -41,6 +41,14 @@ int WINAPI wWinMain( HINSTANCE hInst,HINSTANCE,LPWSTR pArgs,INT )
 			L"\n\nException caught at main window creation.";
 		MessageBox( nullptr,eMsg.c_str(),e.GetExceptionType().c_str(),MB_OK );
 	}
+	catch( const std::exception& e )
+	{
+		// need to convert std::exception what() string from narrow to wide string
+		const std::string whatStr( e.what() );
+		const std::wstring eMsg = std::wstring( whatStr.begin(),whatStr.end() ) +
+			L"\n\nException caught at main window creation.";
+		MessageBox( nullptr,eMsg.c_str(),L"Unhandled STL Exception",MB_OK );
+	}
 	catch( ... )
 	{
 		MessageBox( nullptr,L"\n\nException caught at main window creation.",
