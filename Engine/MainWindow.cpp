8cc5718840931d5d4a3276cf8dcf21508fabea01
planetchili <planetchili@users.noreply.github.com>
2016-07-20 16:01:00 +0900

    text formatting sucks for reference and pointer

--- Engine/MainWindow.cpp
+++ Engine/MainWindow.cpp
@@ -79,7 +79,7 @@ bool MainWindow::IsMinimized() const
 	return IsIconic( hWnd ) != 0;
 }
 
-void MainWindow::ShowMessageBox( const std::wstring & title,const std::wstring & message ) const
+void MainWindow::ShowMessageBox( const std::wstring& title,const std::wstring& message ) const
 {
 	MessageBox( hWnd,message.c_str(),title.c_str(),MB_OK );
 }
